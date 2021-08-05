94b104472d76031fc240a55ea21f093f5132d0c4
Ricardo Costa <ricardo.costa@guardian.co.uk>
2021-08-05 17:12:32 +0100

    JSON encode BrazeTrackRequest

--- src/main/scala/payment_failure_comms/BrazeConnector.scala
+++ src/main/scala/payment_failure_comms/BrazeConnector.scala
@@ -1,31 +1,33 @@
 package payment_failure_comms
 
 import okhttp3.{MediaType, OkHttpClient, Request, RequestBody, Response}
-import payment_failure_comms.models.{BrazeConfig, BrazeRequestFailure, BrazeResponseFailure, Failure}
+import payment_failure_comms.models.{BrazeConfig, BrazeTrackRequest, BrazeRequestFailure, BrazeResponseFailure, Failure}
 import scala.util.Try
+import io.circe.generic.auto._
+import io.circe.syntax._
 
 object BrazeConnector {
 
   val client = new OkHttpClient()
   val JSON: MediaType = MediaType.get("application/json; charset=utf-8")
 
-  def sendCustomEvent(brazeConfig: BrazeConfig, payload: String): Either[Failure, Unit] = {
+  def sendCustomEvent(brazeConfig: BrazeConfig, payload: BrazeTrackRequest): Either[Failure, Unit] = {
     handleRequestResult(
       sendRequest(
         url = s"https://${brazeConfig.instanceUrl}/users/track",
         bearerToken = brazeConfig.bearerToken,
-        payload = payload
+        RequestBody.create(payload.asJson.toString, JSON)
       )
     )
   }
 
-  def sendRequest(url: String, bearerToken: String, payload: String): Either[Throwable, Response] = {
-    val body: RequestBody = RequestBody.create(payload, JSON)
+  def sendRequest(url: String, bearerToken: String, body: RequestBody): Either[Throwable, Response] = {
     val request: Request = new Request.Builder()
       .header("Authorization", s"Bearer ${bearerToken}")
       .url(url)
       .post(body)
       .build()
+
     Try(
       client.newCall(request).execute()
     ).toEither
