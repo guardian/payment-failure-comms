85c603841e33fcf9beb1d2d821d5d7c323e4d7e2
Ricardo Costa <ricardo.costa@guardian.co.uk>
2021-08-05 13:46:17 +0100

    Failure class

---
+++ src/main/scala/payment_failure_comms/models/Failure.scala
@@ -0,0 +1,11 @@
+package payment_failure_comms.models
+
+sealed trait Failure {
+  def kind: String
+  def details: String
+}
+
+case class ConfigFailure(details: String) extends Failure {
+  val kind: String = "Config"
+}
+
