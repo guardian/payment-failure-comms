10b203e183a963cc18e624b4e5f44cbe71e9609e
Ricardo Costa <ricardo.costa@guardian.co.uk>
2021-08-19 16:49:29 +0100

    Test Handler

--- src/main/scala/payment_failure_comms/Handler.scala
+++ src/main/scala/payment_failure_comms/Handler.scala
@@ -1,7 +1,16 @@
 package payment_failure_comms
 
+import payment_failure_comms.models.Config
+
 object Handler {
+
   def handleRequest(): Unit = {
-    println("I totally just ran.")
+    (for {
+      config <- Config()
+    } yield ()) match {
+      case Left(_)  => println("Success")
+      case Right(_) => println("I totally just ran.")
+    }
   }
+
 }
