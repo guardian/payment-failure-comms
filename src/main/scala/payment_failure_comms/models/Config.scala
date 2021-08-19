f0024fce50a5c15d55d3968c766a61205b64a52a
Ricardo Costa <ricardo.costa@guardian.co.uk>
2021-08-19 15:00:38 +0100

    Read Salesforce config

--- src/main/scala/payment_failure_comms/models/Config.scala
+++ src/main/scala/payment_failure_comms/models/Config.scala
@@ -1,6 +1,16 @@
 package payment_failure_comms.models
 
-case class Config(idapi: IdapiConfig, braze: BrazeConfig)
+case class Config(salesforce: SalesforceConfig, idapi: IdapiConfig, braze: BrazeConfig)
+
+case class SalesforceConfig(
+    instanceUrl: String,
+    apiVersion: String,
+    clientId: String,
+    clientSecret: String,
+    username: String,
+    password: String,
+    token: String
+)
 
 case class IdapiConfig(instanceUrl: String, bearerToken: String)
 
@@ -12,12 +22,28 @@ object Config {
       sys.env.get(prop).toRight(ConfigFailure(s"Could not obtain $prop"))
 
     for {
+      salesforceInstanceUrl <- getFromEnv("salesforceInstanceUrl")
+      salesforceApiVersion <- getFromEnv("salesforceApiVersion")
+      salesforceClientId <- getFromEnv("salesforceClientId")
+      salesforceClientSecret <- getFromEnv("salesforceClientSecret")
+      salesforceUsername <- getFromEnv("salesforceUsername")
+      salesforcePassword <- getFromEnv("salesforcePassword")
+      salesforceToken <- getFromEnv("salesforceToken")
       brazeInstanceUrl <- getFromEnv("brazeInstanceUrl")
       brazeBearerToken <- getFromEnv("brazeBearerToken")
       brazeZuoraAppId <- getFromEnv("zuoraAppIdForBraze")
       idapiInstanceUrl <- getFromEnv("idapiInstanceUrl")
       idapiBearerToken <- getFromEnv("idapiBearerToken")
     } yield Config(
+      SalesforceConfig(
+        salesforceInstanceUrl,
+        salesforceApiVersion,
+        salesforceClientId,
+        salesforceClientSecret,
+        salesforceUsername,
+        salesforcePassword,
+        salesforceToken
+      ),
       IdapiConfig(idapiInstanceUrl, idapiBearerToken),
       BrazeConfig(brazeInstanceUrl, brazeBearerToken, brazeZuoraAppId)
     )
