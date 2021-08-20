1f36abe37712b46c4fedb7013f501d8f8a99f9de
Ricardo Costa <ricardo.costa@guardian.co.uk>
2021-08-20 12:27:47 +0100

    Better error prints

--- src/main/scala/payment_failure_comms/models/SFCompositeResponse.scala
+++ src/main/scala/payment_failure_comms/models/SFCompositeResponse.scala
@@ -3,11 +3,12 @@ package payment_failure_comms.models
 case class SFCompositeResponse(responses: Seq[SFResponse]) {
   lazy val hasErrors: Boolean = responses.exists(!_.success)
   lazy val errorsAsString: Option[String] =
-    if (hasErrors) Some(s"Composite Request failed with: ${responses.map(a => a.errorAsString)}") else None
+    if (hasErrors) Some(s"Composite Request failed with: ${responses.filter(!_.success).map(_.errorAsString)}")
+    else None
 }
 
 case class SFResponse(id: Option[String], success: Boolean, errors: Seq[SFResponseError]) {
-  def errorAsString: Option[String] = if (success) None else Some(s"Errors: ${errors.map(a => a.errorAsString)}.")
+  def errorAsString: Option[String] = if (success) None else Some(s"Errors: ${errors.map(_.errorAsString)}.")
 }
 
 case class SFResponseError(statusCode: String, message: String, fields: Seq[String]) {
