99307cb853f96e870570f199115daebd849557d7
Ricardo Costa <ricardo.costa@guardian.co.uk>
2021-09-01 11:54:02 +0100

    Update PaymentFailureRecord

--- src/main/scala/payment_failure_comms/models/PaymentFailureRecord.scala
+++ src/main/scala/payment_failure_comms/models/PaymentFailureRecord.scala
@@ -1,6 +1,12 @@
 package payment_failure_comms.models
 
-// TODO: Add actual fields to this case class
-case class PaymentFailureRecord()
+case class PaymentFailureRecord(
+    Id: String,
+    Contact__r: SFContact,
+    Status__c: String,
+    PF_Comms_Number_of_Attempts__c: Option[Int] = None
+)
+
+case class SFContact(IdentityID__c: String)
 
 case class SFPaymentFailureRecordWrapper(totalSize: Int, done: Boolean, records: Seq[PaymentFailureRecord])
