f64e162c795a48a9e6b7649cb8386a8cd74f6d59
Ricardo Costa <ricardo.costa@guardian.co.uk>
2021-09-21 10:30:30 +0100

    Handler gets Braze IDs

--- src/main/scala/payment_failure_comms/Handler.scala
+++ src/main/scala/payment_failure_comms/Handler.scala
@@ -1,6 +1,12 @@
 package payment_failure_comms
 
-import payment_failure_comms.models.Config
+import payment_failure_comms.models.{
+  Config,
+  Failure,
+  IdapiConfig,
+  PaymentFailureRecord,
+  PaymentFailureRecordWithBrazeId
+}
 
 object Handler {
 
@@ -9,10 +15,24 @@ object Handler {
       config <- Config()
       sfConnector <- SalesforceConnector(config.salesforce)
       records <- sfConnector.getRecordsToProcess()
+      recordsWithBrazeId = augmentRecords(config.idapi, records)
+
     } yield ()) match {
       case Left(failure) => println(failure)
       case Right(_)      => println("I totally just ran.")
     }
+  }
+
+  def augmentRecords(
+      idapiConfig: IdapiConfig,
+      records: Seq[PaymentFailureRecord]
+  ): Seq[PaymentFailureRecordWithBrazeId] = {
+    records.map(record =>
+      PaymentFailureRecordWithBrazeId(
+        record,
+        IdapiConnector.getBrazeId(idapiConfig, record.Contact__r.IdentityID__c)
+      )
+    )
   }
 
 }
