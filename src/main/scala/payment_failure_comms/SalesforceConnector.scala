787fe490240183b27c5a3ed1a8bb14d16a62d448
Kelvin Chappell <kelvin.chappell@theguardian.com>
2021-12-08 10:13:59 +0000

    Send 10 events to Braze

--- src/main/scala/payment_failure_comms/SalesforceConnector.scala
+++ src/main/scala/payment_failure_comms/SalesforceConnector.scala
@@ -70,7 +70,7 @@ object SalesforceConnector {
       |  'Ready to send voluntary cancel event',
       |  'Ready to send auto cancel event'
       |)
-      |LIMIT 1""".stripMargin
+      |LIMIT 10""".stripMargin
 
     handleRequestResult[SFPaymentFailureRecordWrapper](logger)(
       responseToQueryRequest(
