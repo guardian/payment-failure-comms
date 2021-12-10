642590ae3f75b1a27cab379b9d64d0c9b6537669
Kelvin Chappell <kelvin.chappell@theguardian.com>
2021-12-10 14:02:22 +0000

    Refactor some logging code

--- src/main/scala/payment_failure_comms/IdapiConnector.scala
+++ src/main/scala/payment_failure_comms/IdapiConnector.scala
@@ -4,7 +4,7 @@ import com.amazonaws.services.lambda.runtime.LambdaLogger
 import io.circe.Decoder
 import io.circe.generic.auto._
 import io.circe.parser.decode
-import okhttp3.{MediaType, OkHttpClient, Request, Response}
+import okhttp3.{OkHttpClient, Request, Response}
 import payment_failure_comms.models._
 
 import scala.util.Try
@@ -16,18 +16,13 @@ object IdapiConnector {
   def getBrazeId(idapiConfig: IdapiConfig, logger: LambdaLogger)(IdentityId: String): Either[Failure, String] = {
     handleRequestResult[IdapiGetUserResponse](logger)(
       responseToGetRequest(logger)(
-        url = s"https://${idapiConfig.instanceUrl}/user/${IdentityId}",
+        url = s"https://${idapiConfig.instanceUrl}/user/$IdentityId",
         bearerToken = idapiConfig.bearerToken
       )
     ).map(_.user.privateFields.brazeUuid)
   }
 
-  def responseToGetRequest(logger: LambdaLogger)(url: String, bearerToken: String): Either[Throwable, Response] = {
-    val request: Request = new Request.Builder()
-      .header("Authorization", s"Bearer ${bearerToken}")
-      .url(url)
-      .build()
-
+  private def logRequest(logger: LambdaLogger, request: Request): Unit =
     Log.request(logger)(
       service = Log.Service.Idapi,
       description = Some("Read Braze UUID for an Identity ID"),
@@ -35,32 +30,39 @@ object IdapiConnector {
       method = request.method()
     )
 
+  def responseToGetRequest(logger: LambdaLogger)(url: String, bearerToken: String): Either[Throwable, Response] = {
+    val request: Request = new Request.Builder()
+      .header("Authorization", s"Bearer $bearerToken")
+      .url(url)
+      .build()
+    logRequest(logger, request)
     Try(
       http.newCall(request).execute()
     ).toEither
   }
+
+  private def logResponse(logger: LambdaLogger, response: Response, body: String): Unit =
+    Log.response(logger)(
+      service = Log.Service.Idapi,
+      url = response.request().url().toString,
+      method = response.request().method(),
+      responseCode = response.code(),
+      body = Some(body)
+    )
 
   def handleRequestResult[T: Decoder](logger: LambdaLogger)(result: Either[Throwable, Response]): Either[Failure, T] = {
     result
       .left.map(i => IdapiRequestFailure(s"Attempt to contact Braze failed with error: ${i.toString}"))
       .flatMap(response => {
         val body = response.body().string()
-
-        Log.response(logger)(
-          service = Log.Service.Idapi,
-          url = response.request().url().toString,
-          method = response.request().method(),
-          responseCode = response.code(),
-          body = Some(body)
-        )
-
+        logResponse(logger, response, body)
         if (response.isSuccessful) {
           decode[T](body)
             .left.map(decodeError =>
-              IdapiResponseFailure(s"Failed to decode successful response:$decodeError. Body to decode ${body}")
+              IdapiResponseFailure(s"Failed to decode successful response:$decodeError. Body to decode $body")
             )
         } else {
-          Left(IdapiResponseFailure(s"The request to Braze was unsuccessful: ${response.code} - ${body}"))
+          Left(IdapiResponseFailure(s"The request to Braze was unsuccessful: ${response.code} - $body"))
         }
       })
   }
