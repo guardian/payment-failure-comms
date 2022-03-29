33a5d3138992e7c1f316bc28be5b3113a1ce369e
Jonathan Flynn <91546670+jonflynng@users.noreply.github.com>
2022-03-29 14:16:56 +0100

    Make all custom attributes optional (#101)
    * json decoding failure due to null values in some fields, have changed them to optional fields
    
    * replace Option monad with Some
    
    * run scalafmt
    
    * replace option with some
    
    Co-authored-by: Jonathan Flynn <37303488+01jonathanf@users.noreply.github.com>

--- src/main/scala/payment_failure_comms/models/PaymentFailureRecord.scala
+++ src/main/scala/payment_failure_comms/models/PaymentFailureRecord.scala
@@ -9,7 +9,7 @@ case class PaymentFailureRecord(
     SF_Subscription__r: SFSubscription,
     Billing_Account__r: Billing,
     PF_Comms_Status__c: String,
-    Payment_Failure_Type__c: String,
+    Payment_Failure_Type__c: Option[String],
     Initial_Payment__r: SFPayment,
     PF_Comms_Last_Stage_Processed__c: Option[String] = None,
     PF_Comms_Number_of_Attempts__c: Option[Int] = Some(0),
@@ -21,19 +21,19 @@ case class PaymentFailureRecord(
     Cut_Off_Date__c: LocalDate
 )
 
-case class SFPayment(Zuora__GatewayResponseCode__c: String, Zuora__GatewayResponse__c: String)
+case class SFPayment(Zuora__GatewayResponseCode__c: Option[String], Zuora__GatewayResponse__c: Option[String])
 
-case class SFContact(IdentityID__c: Option[String], FirstName: String, LastName: String)
+case class SFContact(IdentityID__c: Option[String])
 
 case class SFSubscription(
     // Although this field should be required, there are records in Salesforce with a null value.
     Product_Name__c: Option[String] = None,
-    Zuora_Subscription_Name__c: String,
+    Zuora_Subscription_Name__c: Option[String],
     Cancellation_Request_Date__c: Option[OffsetDateTime] = None
 )
 
 case class Billing(
-    Zuora__BillToCountry__c: String
+    Zuora__BillToCountry__c: Option[String]
 )
 
 case class SFPaymentFailureRecordWrapper(totalSize: Int, done: Boolean, records: Seq[PaymentFailureRecord])
