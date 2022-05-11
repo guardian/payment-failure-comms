c257013415ec26bb454bd644ab214fd6a808e0ae
Kelvin Chappell <kelvin.chappell@theguardian.com>
2022-05-11 11:32:54 +0100

    Don't try to dedup custom Braze events and attributes (#117)

--- src/test/scala/payment_failure_comms/models/BrazeTrackRequestTest.scala
+++ src/test/scala/payment_failure_comms/models/BrazeTrackRequestTest.scala
@@ -38,14 +38,10 @@ class BrazeTrackRequestTest extends AnyFlatSpec with should.Matchers {
   }
 
   "Apply" should "succeed if no records" in {
-    BrazeTrackRequest(
-      records = Nil,
-      zuoraAppId = "z1",
-      eventsAlreadyWritten = BrazeUserResponse(Nil)
-    ) shouldBe Right(Seq())
+    BrazeTrackRequest(records = Nil, zuoraAppId = "z1") shouldBe Right(Seq())
   }
 
-  val eventProperties = EventProperties(product = "prod1", currency = "GBP", amount = 1.2)
+  private val eventProperties = EventProperties(product = "prod1", currency = "GBP", amount = 1.2)
 
   it should "succeed if all records are valid" in {
     BrazeTrackRequest(
@@ -54,8 +50,7 @@ class BrazeTrackRequestTest extends AnyFlatSpec with should.Matchers {
         mkPaymentFailureRecord(2, "Ready to send voluntary cancel event"),
         mkPaymentFailureRecord(3, "Ready to send auto cancel event")
       ),
-      zuoraAppId = "z1",
-      eventsAlreadyWritten = BrazeUserResponse(Nil)
+      zuoraAppId = "z1"
     ) shouldBe Right(
       Seq(
         BrazeTrackRequest(
@@ -129,8 +124,7 @@ class BrazeTrackRequestTest extends AnyFlatSpec with should.Matchers {
         mkPaymentFailureRecord(11, "Ready to send voluntary cancel event"),
         mkPaymentFailureRecord(12, "Ready to send auto cancel event")
       ),
-      zuoraAppId = "z1",
-      eventsAlreadyWritten = BrazeUserResponse(Nil)
+      zuoraAppId = "z1"
     ) shouldBe Right(
       Seq(
         BrazeTrackRequest(
@@ -336,212 +330,7 @@ class BrazeTrackRequestTest extends AnyFlatSpec with should.Matchers {
         mkPaymentFailureRecord(2, "Unknown status"),
         mkPaymentFailureRecord(3, "Ready to send auto cancel event")
       ),
-      zuoraAppId = "z1",
-      eventsAlreadyWritten = BrazeUserResponse(Nil)
+      zuoraAppId = "z1"
     ) shouldBe Left(SalesforceResponseFailure("Unexpected PF_Comms_Status__c value 'Unknown status' in PF record '2'"))
-  }
-
-  "diff" should "give an empty list if no events" in {
-    val events = Nil
-    val eventsAlreadyWritten = BrazeUserResponse(Nil)
-    BrazeTrackRequest.diff(events, eventsAlreadyWritten) shouldBe Nil
-  }
-
-  it should "give complete list of events if none already written" in {
-    val events = Seq(
-      CustomEventWithAttributes(
-        Seq(),
-        CustomEvent(
-          external_id = "ei1",
-          app_id = "z1",
-          name = "payment_failure",
-          time = "2021-10-26T11:15:27Z",
-          properties = eventProperties
-        )
-      )
-    )
-    val eventsAlreadyWritten = BrazeUserResponse(users =
-      Seq(
-        User(
-          external_id = "ei2",
-          custom_events = Some(
-            Seq(
-              UserCustomEvent(
-                name = "payment_recovery",
-                first = ZonedDateTime.of(2019, 10, 10, 4, 6, 12, 0, ZoneId.of("UTC")),
-                last = ZonedDateTime.of(2020, 2, 3, 4, 5, 6, 0, ZoneId.of("UTC")),
-                count = 3
-              )
-            )
-          )
-        )
-      )
-    )
-    BrazeTrackRequest.diff(events, eventsAlreadyWritten) shouldBe Seq(
-      CustomEventWithAttributes(
-        Seq(),
-        CustomEvent(
-          external_id = "ei1",
-          app_id = "z1",
-          name = "payment_failure",
-          time = "2021-10-26T11:15:27Z",
-          properties = eventProperties
-        )
-      )
-    )
-  }
-
-  it should "miss out events that match those already written" in {
-    val events = Seq(
-      CustomEventWithAttributes(
-        Seq(),
-        CustomEvent(
-          external_id = "ei1",
-          app_id = "z1",
-          name = "payment_failure",
-          time = "2021-10-26T11:15:27Z",
-          properties = eventProperties
-        )
-      )
-    )
-    val eventsAlreadyWritten = BrazeUserResponse(users =
-      Seq(
-        User(
-          external_id = "ei1",
-          custom_events = Some(
-            Seq(
-              UserCustomEvent(
-                name = "payment_failure",
-                first = ZonedDateTime.of(2019, 10, 10, 4, 6, 12, 0, ZoneId.of("UTC")),
-                last = ZonedDateTime.of(2021, 10, 26, 11, 15, 27, 0, ZoneId.of("UTC")),
-                count = 3
-              )
-            )
-          )
-        )
-      )
-    )
-    BrazeTrackRequest.diff(events, eventsAlreadyWritten) shouldBe Nil
-  }
-
-  it should "not miss out events where name doesn't match" in {
-    val events = Seq(
-      CustomEventWithAttributes(
-        Seq(),
-        CustomEvent(
-          external_id = "ei1",
-          app_id = "z1",
-          name = "payment_failure",
-          time = "2021-10-26T11:15:27Z",
-          properties = eventProperties
-        )
-      )
-    )
-    val eventsAlreadyWritten = BrazeUserResponse(users =
-      Seq(
-        User(
-          external_id = "ei1",
-          custom_events = Some(
-            Seq(
-              UserCustomEvent(
-                name = "payment_recovery",
-                first = ZonedDateTime.of(2019, 10, 10, 4, 6, 12, 0, ZoneId.of("UTC")),
-                last = ZonedDateTime.of(2021, 10, 26, 11, 15, 27, 0, ZoneId.of("UTC")),
-                count = 3
-              )
-            )
-          )
-        )
-      )
-    )
-    BrazeTrackRequest.diff(events, eventsAlreadyWritten) shouldBe Seq(
-      CustomEventWithAttributes(
-        Seq(),
-        CustomEvent(
-          external_id = "ei1",
-          app_id = "z1",
-          name = "payment_failure",
-          time = "2021-10-26T11:15:27Z",
-          properties = eventProperties
-        )
-      )
-    )
-  }
-
-  it should "not miss out events where last time of same event was before the time of the event" in {
-    val events = Seq(
-      CustomEventWithAttributes(
-        Seq(),
-        CustomEvent(
-          external_id = "ei1",
-          app_id = "z1",
-          name = "payment_failure",
-          time = "2021-10-26T11:15:27Z",
-          properties = eventProperties
-        )
-      )
-    )
-    val eventsAlreadyWritten = BrazeUserResponse(users =
-      Seq(
-        User(
-          external_id = "ei1",
-          custom_events = Some(
-            Seq(
-              UserCustomEvent(
-                name = "payment_failure",
-                first = ZonedDateTime.of(2019, 10, 10, 4, 6, 12, 0, ZoneId.of("UTC")),
-                last = ZonedDateTime.of(2020, 10, 26, 11, 15, 27, 0, ZoneId.of("UTC")),
-                count = 3
-              )
-            )
-          )
-        )
-      )
-    )
-    BrazeTrackRequest.diff(events, eventsAlreadyWritten) shouldBe Seq(
-      CustomEventWithAttributes(
-        Seq(),
-        CustomEvent(
-          external_id = "ei1",
-          app_id = "z1",
-          name = "payment_failure",
-          time = "2021-10-26T11:15:27Z",
-          properties = eventProperties
-        )
-      )
-    )
-  }
-
-  it should "miss out events where last time of same event was after the time of the event" in {
-    val events = Seq(
-      CustomEventWithAttributes(
-        Seq(SubscriptionIdAttr("b1", Some("A-S87234234"))),
-        CustomEvent(
-          external_id = "ei1",
-          app_id = "z1",
-          name = "payment_failure",
-          time = "2021-10-26T11:15:27Z",
-          properties = eventProperties
-        )
-      )
-    )
-    val eventsAlreadyWritten = BrazeUserResponse(users =
-      Seq(
-        User(
-          external_id = "ei1",
-          custom_events = Some(
-            Seq(
-              UserCustomEvent(
-                name = "payment_failure",
-                first = ZonedDateTime.of(2019, 10, 10, 4, 6, 12, 0, ZoneId.of("UTC")),
-                last = ZonedDateTime.of(2021, 10, 27, 11, 15, 27, 0, ZoneId.of("UTC")),
-                count = 3
-              )
-            )
-          )
-        )
-      )
-    )
-    BrazeTrackRequest.diff(events, eventsAlreadyWritten) shouldBe Nil
   }
 }
