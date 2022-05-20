1a4913ab73ea640960410e7dc34ea11fac98eb37
Kelvin Chappell <kelvin.chappell@theguardian.com>
2022-05-20 15:39:33 +0100

    WIP

--- src/main/scala/payment_failure_comms/Handler2.scala
+++ src/main/scala/payment_failure_comms/Handler2.scala
@@ -1,35 +1,50 @@
 package payment_failure_comms
 
-import payment_failure_comms.Handler.{PartitionedRecords, augmentRecordsWithBrazeId, findBrazeId}
-import payment_failure_comms.models.{PaymentFailureRecord, PaymentFailureRecordWithBrazeId}
-import payment_failure_comms.service.{ConfigLoad, Idapi, LoggingLive, Salesforce, SalesforceLive}
+import com.amazonaws.services.lambda.runtime.LambdaLogger
+import payment_failure_comms.models.{Config, IdapiConfig, PaymentFailureRecord, PaymentFailureRecordWithBrazeId}
+import payment_failure_comms.service._
 import zio._
 
 object Handler2 extends ZIOAppDefault {
 
   private val program =
     for {
+      config <- ZIO.service[Config]
       records <- Salesforce.fetchPaymentFailureRecords
-      augmentedRecords = augmentRecordsWithBrazeId(findBrazeId(config.idapi, logger))(records)
-      _ <- ZIO.foreachDiscard(records)(record => Console.printLine(record))
+      augmentedRecords <- augmentRecordsWithBrazeId(findBrazeId(config.idapi, ConsoleLogger()))(records)
+      _ <- ZIO.foreachDiscard(augmentedRecords)(record => Console.printLine(record))
     } yield ()
 
-  private def augmentRecordsWithBrazeId(records: Seq[PaymentFailureRecord]) = {
-    def r(record: PaymentFailureRecord) =
-      record match {
-        case None => acc.copy(withoutBrazeId = acc.withoutBrazeId :+ record)
-        case Some(brazeId) =>
-          acc.copy(withBrazeId = acc.withBrazeId :+ PaymentFailureRecordWithBrazeId(record, brazeId))
-      }
-    for {
-      x <- ZIO.foldLeft(records)(Nil, Nil)((acc, record) =>
-        Idapi.fetchBrazeId(record) match {
+  private def findBrazeId(idapiConfig: IdapiConfig, logger: LambdaLogger)(
+      record: PaymentFailureRecord
+  ): Option[String] =
+    record.Contact__r.IdentityID__c match {
+      case Some(identityId) =>
+        IdapiConnector.getBrazeId(idapiConfig, logger)(identityId) match {
+          case Left(_)        => None
+          case Right(brazeId) => Some(brazeId)
+        }
+      case None => Some(record.Contact__c)
+    }
+
+  private case class PartitionedRecords(
+      withBrazeId: Seq[PaymentFailureRecordWithBrazeId],
+      withoutBrazeId: Seq[PaymentFailureRecord]
+  )
+
+  private def augmentRecordsWithBrazeId(
+      findBrazeId: PaymentFailureRecord => Option[String]
+  )(records: Seq[PaymentFailureRecord]) = {
+    val w = for {
+      x <- ZIO.foldLeft(records)(PartitionedRecords(Nil, Nil))((acc, record) =>
+        ZIO.attempt(findBrazeId(record)) map {
           case None => acc.copy(withoutBrazeId = acc.withoutBrazeId :+ record)
           case Some(brazeId) =>
             acc.copy(withBrazeId = acc.withBrazeId :+ PaymentFailureRecordWithBrazeId(record, brazeId))
         }
       )
     } yield x
+    w
   }
 
   override def run: ZIO[ZIOAppArgs, Any, Any] =
